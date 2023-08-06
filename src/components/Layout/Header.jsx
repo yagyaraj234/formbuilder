35c0ac7c9221cb33e649d0f8add0fa5d51d9b82a
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-06 23:55:16 +0530

    header added

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -6,32 +6,32 @@ const Header = () => {
     <>
       <div className=" flex justify-between items-center py-1 px-10">
         <div className="flex items-center">
-          <p className="h-3 w-12 bg-[#A40E4C] rounded-lg mr-2"></p>
+          <p className="h-8 w-10 bg-[#f405dc] rounded-r-xl mr-2 opacity-40"></p>
           <h1 className="text-4xl font-semibold">Form Builder</h1>
         </div>
         <div className="flex gap-10 font-semibold ">
           <NavLink
-            className="hover:text-gray-700 duration-300 transition-colors delay-75"
+            className="hover:text-[#A40E4C] duration-300 transition-colors delay-75"
             to="/"
           >
             Home
           </NavLink>
           <NavLink
-            className="hover:text-gray-700 duration-300 transition-colors delay-75"
+            className="hover:text-[#A40E4C] duration-300 transition-colors delay-75 "
             to="/form-builder"
           >
             Form Builder
           </NavLink>
           <NavLink
-            className="hover:text-gray-700 duration-300 transition-colors delay-75"
+            className="hover:text-[#A40E4C] duration-300 transition-colors delay-75"
             to="/contact"
           >
             Contact
           </NavLink>
         </div>
         <div className=" flex gap-10">
-          <button className="font-semibold">Login</button>
-          <button className="bg-black text-white px-4 py-1 rounded-xl hover:bg-gray-800 transition-colors duration-300 delay-100 font-semibold">
+          <button className="font-semibold cursor-not-allowed">Login</button>
+          <button className="bg-black text-white px-4 py-1 rounded-xl hover:bg-gray-800 transition-colors duration-300 delay-100 font-semibold cursor-not-allowed">
             Register
           </button>
         </div>
