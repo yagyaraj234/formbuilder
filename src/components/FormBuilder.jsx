8d2f63583878ca17e3a3ef75d297f64cb3b0117c
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-07 00:47:56 +0530

    types button added

--- src/components/FormBuilder.jsx
+++ src/components/FormBuilder.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Layout from "./Layout/Layout";
-import QuestionType from "./FormBuilder";
+import QuestionType from "./QuestionType";
 
 const FormBuilder = () => {
   // const [questionType, setQuestionType] = useState("");
@@ -27,12 +27,9 @@ const FormBuilder = () => {
 
   return (
     <Layout>
-      {/* <QuestionType onSelectType={handleSelectType} /> */}
-      {/* <button></button>
-       */}
       <div className="p-10 border m-10 rounded-xl">
         <div className="flex justify-between">
-          <div>
+          <div className="flex ">
             <input
               type="text"
               className="border-b outline-none"
@@ -40,10 +37,12 @@ const FormBuilder = () => {
               onChange={(e) => setFormName(e.target.value)}
               placeholder="Enter Project name"
             />
-            <ion-icon
-              onClick={() => setFormName("")}
-              name="close-outline"
-            ></ion-icon>
+            <div className="mt-2">
+              <ion-icon
+                onClick={() => setFormName("")}
+                name="close-outline"
+              ></ion-icon>
+            </div>
           </div>
 
           <button className="bg-[#A40E4C] text-white font-semibold px-3 py-1 rounded-xl">
--- src/components/QuestionType.jsx
+++ src/components/QuestionType.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
+const button = [
+  { name: "Categorize", id: 1 },
+  { name: "Cloze", id: 2 },
+  { name: "comprehension", id: 3 },
+];
+
 const QuestionType = () => {
+  console.log(button);
   return (
-    <div className="flex md:w-[50%]">
-      <div>
-        <ion-icon name="add-outline"></ion-icon>
-        <button>Categorized</button>
-      </div>
+    <div className="flex md:w-[50%] py-4 gap-10">
+      {/* Question button  */}
+      {button.map((button) => (
+        <div
+          key={button.id}
+          className="bg-[#A40E4C] text-white rounded-xl font-semibold py-1 px-4 flex"
+        >
+          <div className="pt-1">
+            <ion-icon name="add-circle-outline"></ion-icon>
+          </div>
+          <button className="pl-2">{button.name}</button>
+        </div>
+      ))}
     </div>
   );
 };
