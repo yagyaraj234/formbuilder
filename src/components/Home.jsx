b7215ecad9851e5fd259511d231f7ea07350946e
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-07 00:55:15 +0530

    title changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,23 +6,28 @@ const Home = () => {
   return (
     <Layout>
       <div className="px-10 grid grid-cols-2">
-        <div className=" min-h-[80vh] pt-20 ">
+        <div className=" min-h-[80vh] pt-20  ">
           <p className="text-6xl font-extrabold text-[#A40E4C]">
-            Empower Your Creativity:
+            Empower Your Creativity :
           </p>
-          <p className="text-4xl font-bold"> Build Forms with Ease</p>
+          <p className="text-4xl font-bold"> Build Forms with Ease and</p>
           <p className="text-4xl font-bold">
             {" "}
-            and Precision using Our Intuitive Web App!"
+            Precision using Our Intuitive <br /> Web App!"
           </p>
 
-          <div className="mt-10 ">
+          <div className="mt-10 flex ">
             <Link
-              className="font-semibold text-xl border border-black rounded-xl px-5 py-1 hover:bg-[#A40E4C] duration-300 transition-colors hover:text-white"
+              className="font-semibold text-xl border border-black rounded-xl px-5 py-1 hover:bg-[#A40E4C] duration-700 transition-colors hover:text-white hover:border-none"
               to="/form-builder"
             >
               {" "}
-              Try it Out
+              <div className="flex gap-2">
+                Try it Out
+                <div>
+                  <ion-icon name="open-outline"></ion-icon>
+                </div>
+              </div>
             </Link>
           </div>
         </div>
