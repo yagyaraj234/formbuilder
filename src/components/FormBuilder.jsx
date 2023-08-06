e612aa600651bc08f965a1ce90843680930dfd1d
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-06 23:55:47 +0530

    form builder component created

--- src/components/FormBuilder.jsx
+++ src/components/FormBuilder.jsx
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "./Layout/Layout";
+import QuestionType from "./FormBuilder";
 
 const FormBuilder = () => {
-  return <Layout></Layout>;
+  // const [questionType, setQuestionType] = useState("");
+
+  // const [currentQuestion, setCurrentQuestion] = useState({
+  //   question: "",
+  //   options: [],
+  //   text: "",
+  //   blanks: [],
+  //   passage: "",
+  // });
+  // const handleSelectType = (type) => {
+  //   setQuestionType(type);
+  //   setCurrentQuestion({
+  //     question: "",
+  //     options: [],
+  //     text: "",
+  //     blanks: [],
+  //     passage: "",
+  //   });
+  // };
+
+  const [formName, setFormName] = useState("Untitled Project");
+
+  return (
+    <Layout>
+      {/* <QuestionType onSelectType={handleSelectType} /> */}
+      {/* <button></button>
+       */}
+      <div className="p-10 border m-10 rounded-xl">
+        <div className="flex justify-between">
+          <div>
+            <input
+              type="text"
+              className="border-b outline-none"
+              value={formName}
+              onChange={(e) => setFormName(e.target.value)}
+              placeholder="Enter Project name"
+            />
+            <ion-icon
+              onClick={() => setFormName("")}
+              name="close-outline"
+            ></ion-icon>
+          </div>
+
+          <button className="bg-[#A40E4C] text-white font-semibold px-3 py-1 rounded-xl">
+            Show Preview
+          </button>
+        </div>
+        <QuestionType />
+      </div>
+    </Layout>
+  );
 };
 
 export default FormBuilder;
---
+++ src/components/QuestionType.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const QuestionType = () => {
+  return (
+    <div className="flex md:w-[50%]">
+      <div>
+        <ion-icon name="add-outline"></ion-icon>
+        <button>Categorized</button>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionType;
