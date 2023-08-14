2e51a72052c83ca7eff4973b577302d5d75e3917
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-14 17:59:24 +0530

    styling changes

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-      <div className=" flex justify-between items-center py-1 px-10">
+      <div className=" flex justify-between items-center py-2 px-10">
         <div className="flex items-center">
-          <p className="h-8 w-10 bg-[#A40E4C] rounded-r-xl mr-2 "></p>
-          <h1 className="text-4xl font-semibold">Form Builder</h1>
+          <p className="h-8 w-10 bg-primary rounded-r-xl mr-2 "></p>
+          <Link to="/" className="text-4xl font-semibold">
+            Form Builder
+          </Link>
         </div>
         <div className="flex gap-10 font-semibold ">
           <NavLink
@@ -29,11 +31,19 @@ const Header = () => {
             Contact
           </NavLink>
         </div>
-        <div className=" flex gap-10">
-          <button className="font-semibold cursor-not-allowed">Login</button>
-          <button className="bg-black text-white px-4 py-1 rounded-xl hover:bg-gray-800 transition-colors duration-300 delay-100 font-semibold cursor-not-allowed">
+        <div className=" flex gap-10  ">
+          <NavLink
+            to="/login"
+            className="font-semibold px-4 py-1 hover:text-primary "
+          >
+            Login
+          </NavLink>
+          <NavLink
+            to="/signup"
+            className="bg-primary text-white px-4 py-1 rounded-xl hover:bg-white hover:text-primary hover:border-primary border-2 border-white hover:border-2 transition-colors duration-300 delay-100 font-semibold "
+          >
             Register
-          </button>
+          </NavLink>
         </div>
       </div>
       <p className="border-b border-gray-300 opacity-30"></p>
