378e52b8d46fd6323e2f32e7bf781d8e09a00074
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-14 17:53:53 +0530

    styling changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
 import FormBuilder from "./components/FormBuilder";
 import Contact from "./components/Contact";
+import Preview from "./components/Preview";
+import Login from "./components/pages/Login";
+import Signup from "./components/pages/Signup";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/form-builder" element={<FormBuilder />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/preview" element={<Preview />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
 
       {/* <Home /> */}
