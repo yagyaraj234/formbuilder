8fe9fc5f5ade1db4db2cc1464cc2113285ac24d4
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-17 16:15:15 +0530

    User model created for user registrations

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema({
     },
   },
   password: { type: String, required: true, minlength: 8 },
+  confirmpassword: { type: String, required: true,  },
   role: { type: String, enum: ["user", "admin"], default: "user" },
   createdAt: { type: Date, default: Date.now() },
 });
